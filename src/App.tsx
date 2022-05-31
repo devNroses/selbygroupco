140ec266cb61c62e656115ff3fb70471706927b4
JonSelby Co <jons@selbygroupco.com>
2022-05-30 17:49:03 -0700

    initial commit with header

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './style/main.scss'
+import Header from './components/Header'
+function App() {
+  return (
+    <div>
+     <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header: React.FC = ({}) => <header className="header-wrapper">
+    <div className="header-status">Work Availability</div>
+    <div className="header-logo">Logo Here</div>
+    <div className="header-social">
+     <ul>
+         <li>Twitter</li>
+         <li>Instagram</li>
+         <li>LinkedIn</li>
+     </ul>
+    </div>
+</header>
+
+export default Header
