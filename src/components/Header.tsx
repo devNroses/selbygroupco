b764785fc8de7b6e9a39824e17382f1b5c4eb31b
JonSelby Co <jons@selbygroupco.com>
2022-06-13 00:35:36 -0700

    added css animation to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import 'animate.css'
 
-const Header: React.FC = ({}) => <header className="header-wrapper">
+const Header: React.FC = ({}) => <header className="header-wrapper animate__animated animate__bounce ">
     <div className="header-status">Work Availability</div>
-    <div className="header-logo">Logo Here</div>
+    <div className="header-logo"><span>Selby Group Co.</span></div>
     <div className="header-social">
      <ul>
          <li>Twitter</li>
