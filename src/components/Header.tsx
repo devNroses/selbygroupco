e25b12b318f4701c7af74f1e12c7bf45ed00f1cf
JonSelby Co <jons@selbygroupco.com>
2022-06-13 18:53:59 -0700

    added header animation to skeleton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import 'animate.css'
 
-const Header: React.FC = ({}) => <header className="header-wrapper animate__animated animate__bounce ">
-    <div className="header-status">Work Availability</div>
-    <div className="header-logo"><span>Selby Group Co.</span></div>
-    <div className="header-social">
+const Header: React.FC = ({}) => <header className="header-wrapper">
+    <div className="header-status animate__animated animate__zoomInDown animate__delay-1s">Work Availability</div>
+    <div className="header-logo animate__animated animate__zoomInDown animate__delay-2s"><span>Selby Group Co.</span></div>
+    <div className="header-social animate__animated animate__zoomInDown animate__delay-3s">
      <ul>
          <li>Twitter</li>
          <li>Instagram</li>
